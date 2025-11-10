aeeba6c9c58900995d16afcbbde2b5782103871f
TheVargas-17 <23308060610434@cetis61.edu.mx>
2025-11-10 10:14:01 -0500

    update

--- nutriapp/app.py
+++ nutriapp/app.py
@@ -1,5 +1,55 @@
-from flask import Flask, render_template
+
+from datetime import datetime
+
+from flask import Flask, render_template,request,redirect
 app = Flask(__name__)
+
+
+usuarios = {}
+
+@app.route('/registro', methods=['GET', 'POST'])
+def registro():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        apellidos = request.form['apellidos']
+        correo = request.form['correo']
+        password = request.form['password']
+        fecha_nacimiento = request.form['fecha_nacimiento']
+        sexo = request.form['sexo']
+        peso = request.form['peso']
+        altura = request.form['altura']
+        actividad_fisica = request.form['actividad_fisica']
+        objetivos = request.form['objetivos']
+        alergias = request.form.getlist('alergias')
+        intolerancias = request.form.getlist('intolerancias')
+        dietas = request.form.getlist('dietas')
+        alimentos_no_gustan = request.form['alimentos_no_gustan']
+        nivel_cocina = request.form['nivel_cocina']
+
+        if correo in usuarios:
+            return render_template('registro.html', error="El correo ya está registrado.")
+        
+        usuarios[correo] = {
+            'nombre': nombre,
+            'apellidos': apellidos,
+            'correo': correo,
+            'password': password,
+            'fecha_nacimiento': fecha_nacimiento,
+            'sexo': sexo,
+            'peso': peso,
+            'altura': altura,
+            'actividad_fisica': actividad_fisica,
+            'objetivos': objetivos,
+            'alergias': alergias,
+            'intolerancias': intolerancias,
+            'dietas': dietas,
+            'alimentos_no_gustan': alimentos_no_gustan,
+            'nivel_cocina': nivel_cocina
+        }
+
+        return redirect('/') 
+
+    return render_template('registro.html')
 
 @app.route('/')
 def index():
@@ -20,6 +70,9 @@ def imc():
 @app.route('/acerca')
 def acerca():
     return render_template('acerca.html')
+@app.route('/perfil')
+def perfil():
+    return render_template('perfil.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
